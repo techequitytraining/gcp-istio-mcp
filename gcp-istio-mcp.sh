770ac5d38528c8de4dea345085708f607bf1246b
shiyghannavti <136981389+shiyghannavti@users.noreply.github.com>
2024-07-21 22:56:56 +0100

    Add option to switch between modes

--- gcp-istio-mcp.sh
+++ gcp-istio-mcp.sh
@@ -88,6 +88,7 @@ cat<<EOF
 Menu for Dual Control Plane Istio Service Mesh Configuration  
 --------------------------------------------------------------
 Please enter number to select your choice:
+(0) Switch between Preview, Execute and Delete modes
 (1) Install tools
 (2) Enable APIs
 (3) Create network
